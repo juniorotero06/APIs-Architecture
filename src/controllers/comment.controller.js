070a690ec748422dddaf01b555c3391cf2039312
JuniorOtero <dgrotero697@gmail.com>
2023-02-20 16:59:07 -0500

    controllers added

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,41 @@
+let _commentService = null;
+
+class CommentController {
+    constructor({ CommentService }){
+        _commentService = CommentService;
+    }
+
+    async get(req, res){
+        const { commentId } = req.params;
+        const comment = await _commentService.get(commentId);
+        return res.send(comment);
+    }
+
+    async update(req, res) {
+        const { body } = req;
+        const { commentId } = req.parms;
+        const updatedComment = await _commentService.update(commentId, body);
+        return res.send(updatedComment);
+    }
+
+    async delete(req, res) {
+        const { commentId } = req.parmas;
+        const deleteComment = await _commentService.delete(commentId);
+        return res.send(deleteComment);
+    }
+
+    async getIdeasComments(req, res){
+        const { ideaId } = req.params;
+        const comments = await _commentService.getIdeasComments(ideaId);
+        return res.send(comments);
+    }
+
+    async createComment(){
+        const { body } = req;
+        const { ideaId } = req.params;
+        const createdComment = await _commentService.createdComment(body, ideaId);
+        return res.status(201).send(createdComment);
+    }
+}
+
+module.exports = CommentController;
--- src/startup/conteiner.js
+++ src/startup/conteiner.js
@@ -2,7 +2,7 @@ const { createContainer, asClass, asValue, asFunction } = require("awilix");
 // Services
 const { HomeService, UserService, IdeaService, CommentService } = require("../services");
 //Controllers
-const { HomeController } = require("../controllers");
+const { HomeController, UserController, IdeaController, CommentController } = require("../controllers");
 //Models
 const { User, Idea, Comment } = require("../models")
 //Repositorie
@@ -30,6 +30,9 @@ container
   })
   .register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton(),
+    UserController: asClass(UserController.bind(UserController)).singleton(),
+    IdeaController: asClass(IdeaController.bind(IdeaController)).singleton(),
+    CommentController: asClass(CommentController.bind(CommentController)).singleton()
   })
   .register({
     HomeRoutes: asFunction(HomeRoutes).singleton(),
