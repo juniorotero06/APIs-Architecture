ffbdaa59c4cdacf694d112ad9bb6e140eeae4198
Junior Otero <dgrotero06@gmail.com>
2023-02-22 22:06:16 -0500

    [ADD] Pagination Added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,34 +1,34 @@
 let _userService = null;
 
 class UserController {
-    constructor({ UserService }){
-        _userService = UserService;
-    }
+  constructor({ UserService }) {
+    _userService = UserService;
+  }
 
-    async get(req, res){
-        const { userId } = req.params;
-        const user = await _userService.get(userId);
-        return res.send(user);
-    }
+  async get(req, res) {
+    const { userId } = req.params;
+    const user = await _userService.get(userId);
+    return res.send(user);
+  }
 
-    async getAll(req, res) {
-        const users = await _userService.getAll();
-        return res.send(users);
-    }
+  async getAll(req, res) {
+    const { pageSize, pageNum } = req.query;
+    const users = await _userService.getAll(pageSize, pageNum);
+    return res.send(users);
+  }
 
-    async update(req, res) {
-        const { body } = req;
-        const { userId } = req.parms;
-        const updatedUser = await _userService.update(userId, body);
-        return res.send(updatedUser);
-    }
+  async update(req, res) {
+    const { body } = req;
+    const { userId } = req.parms;
+    const updatedUser = await _userService.update(userId, body);
+    return res.send(updatedUser);
+  }
 
-    async delete(req, res) {
-        const { userId } = req.parmas;
-        const deleteUser = await _userService.delete(userId);
-        return res.send(deleteUser);
-    }
-
+  async delete(req, res) {
+    const { userId } = req.parmas;
+    const deleteUser = await _userService.delete(userId);
+    return res.send(deleteUser);
+  }
 }
 
 module.exports = UserController;
